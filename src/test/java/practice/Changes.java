56b4b43d41bc7930b0974cf95cdc25745c4fd268
kisha <kisha@DESKTOP-U8GE9MR>
2023-04-04 10:03:13 +0530

    Chang

--- src/test/java/practice/Changes.java
+++ src/test/java/practice/Changes.java
@@ -3,7 +3,7 @@ package practice;
 public class Changes {
 
 	public static void main(String[] args) {
-    System.out.println("I makes some changes");
+    System.out.println("I makes some changess");
 	}
 
 }
