2a74a0243c615361018d2181491f69a89efb0290
NPM <NPM@Administrator>
2023-04-07 10:14:41 +0530

    newtest

---
+++ src/test/java/Patient/NewTest.java
@@ -0,0 +1,19 @@
+package Patient;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+@Test
+public void m2()
+{
+	String BROWSER = "https://npm.com";
+	String URL="url";
+	System.out.println(URL);
+	System.out.println(BROWSER);
+
+
+
+}
+	
+}
+est
